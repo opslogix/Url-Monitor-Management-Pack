ef4024975213d2aacc99aac079d673ea33769798
jsprvdb <jasper@opslogix.com>
2019-06-13 13:52:55 +0200

    Minor format change

--- OpsLogix.IMP.Url.ConfigurationControl/UrlMonitoringConfigurationContainer.cs
+++ OpsLogix.IMP.Url.ConfigurationControl/UrlMonitoringConfigurationContainer.cs
@@ -1,12 +1,10 @@
-﻿using Microsoft.EnterpriseManagement;
-using Microsoft.EnterpriseManagement.Configuration;
+﻿using Microsoft.EnterpriseManagement.Configuration;
 using Microsoft.EnterpriseManagement.Mom.Internal.UI;
 using OpsLogix.IMP.Url.ConfigurationControl.Controls;
 using OpsLogix.IMP.Url.ConfigurationControl.Models;
 using OpsLogix.IMP.Url.ConfigurationControl.Presenters;
 using OpsLogix.IMP.Url.Shared.Services;
 using System;
-using System.Diagnostics;
 
 namespace OpsLogix.IMP.Url.ConfigurationControl
 {
@@ -14,19 +12,20 @@ namespace OpsLogix.IMP.Url.ConfigurationControl
     {
         private readonly Guid _urlMonitoringAddressClassId = new Guid("7072898d-75b3-9a8b-20ed-404021f93c60");
 
-    public UrlMonitoringConfigurationContainer()
-    {
-      InitializeComponent();
-    }
+        public UrlMonitoringConfigurationContainer()
+        {
+            InitializeComponent();
+        }
 
-    protected override void OnLoad(EventArgs e)
-    {
-      base.OnLoad(e);
-      ScomSdkService scomSdkService = new ScomSdkService(ManagementGroup);
-      UrlMonitoringConfigurationModel urlConfigurationModel = new UrlMonitoringConfigurationModel(scomSdkService, _urlMonitoringAddressClassId, SystemMonitoringClass.HealthService.Id);
+        protected override void OnLoad(EventArgs e)
+        {
+            var scomSdkService = new ScomSdkService(base.ManagementGroup);
+            var urlConfigurationModel = new UrlMonitoringConfigurationModel(scomSdkService, _urlMonitoringAddressClassId, SystemMonitoringClass.HealthService.Id);
 
-      new UrlMonitoringConfigurationPresenter(urlMonitoringConfigurationControl, urlConfigurationModel);
-      new UrlMonitoringConfigurationDialogPresenter(new UrlMonitoringConfigurationDialogForm(), urlConfigurationModel);
+            new UrlMonitoringConfigurationPresenter(urlMonitoringConfigurationControl, urlConfigurationModel);
+            new UrlMonitoringConfigurationDialogPresenter(new UrlMonitoringConfigurationDialogForm(), urlConfigurationModel);
+
+            base.OnLoad(e);
+        }
     }
-  }
 }
